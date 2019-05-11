dd931039732af44ed40fadb7fec164ff9b2404bb
Ekow Abaka Ainooson <jainooson@gmail.com>
2019-05-11 13:22:11 -0500

    Improving web interface interactions

--- src/instagram.py
+++ src/instagram.py
@@ -1,16 +1,45 @@
 from selenium import webdriver
-from selenium.webdriver.chrome.options import Options
+from selenium.common.exceptions import NoSuchElementException
+import sys
 
 
-class Interface(object):
+def ensure_login(func):
+    """
+    Decorator to ensure that logins are enforced before any other actions
+    :param func:
+    :return:
+    """
+
+    def enforcer(instance, image_path, message):
+        if WebUIInterface.logged_in:
+            func(instance, image_path, message)
+        else:
+            print("You are currently not logged in!", file=sys.stderr)
+
+    return enforcer
+
+
+class WebUIInterface(object):
+
+    logged_in = False
 
     def __init__(self, username, password, headless=False):
+        """
+        Create an instance of the instagram interface
+        :param username:
+        :param password:
+        :param headless:
+        """
         self.username = username
         self.password = password
         self.headless = headless
         self.browser = None
 
     def login(self):
+        """
+        Loginto the instagram web UI
+        :return:
+        """
         profile = webdriver.FirefoxProfile()
         profile.set_preference(
             "general.useragent.override",
@@ -34,6 +63,28 @@ class Interface(object):
         element = self.browser.find_element_by_xpath("//div[text()='Log In']")
         element.click()
 
-        element = self.browser.find_element_by_xpath("//button[text()='Not Now']")
-        element.click()
+        try:
+            element = self.browser.find_element_by_xpath("//button[text()='Not Now']")
+            element.click()
+            element = self.browser.find_element_by_xpath("//span[@aria-label='Profile']")
+            element = element.find_element_by_xpath("./..")
+            element.click()
+            WebUIInterface.logged_in = True
+        except NoSuchElementException:
+            try:
+                element = self.browser.find_element_by_xpath("//span[@aria-label='Profile']")
+                element = element.find_element_by_xpath("./..")
+                element.click()
+                WebUIInterface.logged_in = True
+            except NoSuchElementException:
+                WebUIInterface.logged_in = False
+                raise ConnectionError("Logging into instagram failed!")
 
+    @ensure_login
+    def post(self, image_path, message):
+        element = self.browser.find_element_by_xpath("//span[@aria-label='New Post']")
+        element = element.find_element_by_xpath("./..")
+        element.click()
+        element = self.browser.find_elements_by_xpath("//input[@type='file']")
+        for file in element:
+            file.send_keys(image_path)
--- src/main.py
+++ src/main.py
@@ -8,8 +8,9 @@ def main():
     parser.add_argument('--password', type=str, nargs=1, required=True)
     args = parser.parse_args()
 
-    insta = instagram.Interface(args.username, args.password)
+    insta = instagram.WebUIInterface(args.username, args.password)
     insta.login()
+    insta.post("/home/ekow/img004.jpg", "First automated test post")
 
 
 if __name__ == "__main__":
